dcb0d2c878939502db300716820c33a6346c1483
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-01-05 11:18:52 +0000

    Update Tasks.js

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -2,16 +2,16 @@ import Task from "./Task"
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
-		<>
-		{tasks.map((task) => (
-			<Task 
-				key={task._ID} 
-				task={task}
-				onDelete={onDelete} 
-				onToggle={onToggle}
-			/>
-		))}
-		</>
+    <>
+    {tasks.map((task) => (
+      <Task 
+        key={task._ID} 
+        task={task}
+        onDelete={onDelete} 
+        onToggle={onToggle}
+      />
+    ))}
+    </>
   )
 }
 
